bfef24221a7c5090b2809760cccf6593f50eacba
Matt Smith <matt@offtopica.uk>
2020-02-05 09:25:44 +0000

    monero: Add testnet and stagenet constants

--- src/main/java/uk/offtopica/addressutil/monero/MoneroNetworkConstants.java
+++ src/main/java/uk/offtopica/addressutil/monero/MoneroNetworkConstants.java
@@ -2,6 +2,8 @@ package uk.offtopica.addressutil.monero;
 
 public class MoneroNetworkConstants {
     public static final MoneroNetworkConstants MAINNET = new MoneroNetworkConstants((byte) 0x12, (byte) 0x2a, (byte) 0x13);
+    public static final MoneroNetworkConstants TESTNET = new MoneroNetworkConstants((byte) 0x35, (byte) 0x3f, (byte) 0x36);
+    public static final MoneroNetworkConstants STAGENET = new MoneroNetworkConstants((byte) 0x18, (byte) 0x24, (byte) 0x19);
 
     private final byte networkByteStandard;
     private final byte networkByteSubaddress;
